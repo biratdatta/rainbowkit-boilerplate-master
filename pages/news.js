b268868bcf044458cffab64a164843ca13cbbe3a
biratdatta <birat.datta26@gmail.com>
2022-12-02 23:22:51 +0530

    first commit

---
+++ pages/news.js
@@ -0,0 +1,12 @@
+import { Footer, Navbar } from '../components';
+import { Feedback } from '../sections';
+
+const News = () => (
+  <div className="bg-primary-black overflow-hidden">
+    <Navbar />
+    <Feedback />
+    <Footer />
+  </div>
+);
+
+export default News;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+function App() {
+  return (
+    <div className="App">
+        <ConnectButton/>
+    </div>
+  );
+}
+
+export default App;
